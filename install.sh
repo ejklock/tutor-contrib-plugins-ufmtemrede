dbafa181a7b6eaed174ea7f3bd0efd2427fc05e5
Evaldo Klock <neto.nemesis@gmail.com>
2024-06-10 15:37:12 -0400

    feat: add install sh

--- install.sh
+++ install.sh
@@ -5,3 +5,14 @@ echo "Copying files"
 cp -r ./* "$(tutor plugins printroot)"
 echo "Done"
 echo "Now run tutor plugins list and tutor plugins enable <plugin_name>"
+echo "To enable all mfe plugins: tutor plugins enable mfe-account mfe-authn mfe-discussions mfe-learner-dashboard mfe-learning mfe-profile"
+
+echo "available plugins:"
+for f in $(ls *.py); do
+    echo "tutor plugins install $(basename $f .py)"
+done
+
+
+
+
+
